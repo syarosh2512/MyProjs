a63c5ff56c0d21f7b2f8dc01d5cbefd2f69611e2
Sergiy Yarosh <sergiy.yarosh@gmail.com>
2025-04-16 13:27:06 +0300

    Python commit 16-04 first

--- PycharmProjects/MyFirst/main.py
+++ PycharmProjects/MyFirst/main.py
@@ -1,14 +1,31 @@
-student= {
-    "name":"<Jack>",
-    "age":20,
-    "is_new":True,
+# student= {
+#     "name":"<Jack>",
+#     "age":20,
+#     "is_new":True,
+#
+# }
+#
+# print("Jon's book")
+# print('Jon\'s book')
+#
+# #math
+from operator import truediv
 
-}
+num = [2,3,4,5]
+num.append (9)
+num=num*2
+print(type(num),"list is new",num)
+num1 = [21,31,41,51]
+boll = True
+print(boll, num1+num)
 
-print("Jon's book")
-print('Jon\'s book')
+world = "circumstances"
+print(world)
 
-#math
+a = int(input("Enter a number 1:"))
+b = int(input("Enter a number 2:"))
+c = float(input("Enter a number 3:"))
 
-
-
+k = a+b+c
+print("result is = ", type(k))
+print("result is = ", a+b+c)
