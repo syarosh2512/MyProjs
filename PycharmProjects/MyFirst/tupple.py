14d19df9ddbdcfca8586474586d40076575651ad
Sergiy Yarosh <sergiy.yarosh@gmail.com>
2025-04-28 16:40:36 +0300

    pthon study

---
+++ PycharmProjects/MyFirst/tupple.py
@@ -0,0 +1,21 @@
+# fruits = ('apple', 'banana', 'cherry')
+# num = (1, 5, 7, 9, 2, 7, 9)
+# mixed = fruits + num
+# print(mixed, type(mixed))
+# for fruit in fruits:
+#     print(fruit)
+#
+# for n in num:
+#     print(n)
+#
+# print(num.count(9))
+#
+# a,b,c,d,e,f,g = (145,2,3,4,5,6,7)
+# print(a)
+
+data = (5, 45, 23,77, True, "text",899)
+
+print(data, type(data))
+print(data[::-1])
+print(data[1:4])
+print(len(data))
